e697ce62ff0377da383e71ab3b516898725d8181
ammon <ammonsawesome@gmail.com>
2025-11-10 15:06:22 -0700

    Fixed onscroll function

--- GRIT/app/(tabs)/ScrollVideos.tsx
+++ GRIT/app/(tabs)/ScrollVideos.tsx
@@ -8,10 +8,9 @@ import Animated, {
   useSharedValue,
   useAnimatedStyle,
   withTiming,
+  runOnJS,
 } from 'react-native-reanimated';
 import { WebView } from 'react-native-webview';
-
-import { scheduleOnRN } from 'react-native-worklets';
 
 import { useState } from 'react';
 
@@ -24,6 +23,12 @@ function createEmbedUrl(vidId: string) {
 export default function ScrollVideos() {
   const [videoId, setVideoId] = useState("t34muYc261o");
   const [embedUrl, setEmbedUrl] = useState(`https://www.youtube.com/shorts/t34muYc261o?controls=1&autoplay=0`);
+
+  // Helper to update both videoId and embedUrl on the JS thread
+  const updateVideo = (id: string) => {
+    setVideoId(id);
+    setEmbedUrl(createEmbedUrl(id));
+  };
 
   const YouTubeEmbed = () => {
 
@@ -43,18 +48,18 @@ export default function ScrollVideos() {
   // const position = useSharedValue(0);
   const flingUp = Gesture.Fling()
     .direction(Directions.UP)
-    .onStart((e) => {
+    .onStart(() => {
       //position.value = withTiming(position.value - 100, { duration: 100 });
-      console.log("Swiped up");
-      setVideoId("3KtWQJuRSmI"); // ScheduleOnRN did not fix the crash
-      setEmbedUrl(createEmbedUrl(videoId));
+      console.log('Swiped up');
+      // runOnJS ensures we call JS-thread state setters from the worklet
+      runOnJS(updateVideo)('3KtWQJuRSmI');
     });
     const flingDown = Gesture.Fling()
         .direction(Directions.DOWN)
         .onStart((e) => {
             console.log("Swiped down");
-            setVideoId("Ll0RattR1DE"); // Change when we make an API call
-            setEmbedUrl(createEmbedUrl(videoId));
+            // Use runOnJS to safely update React state from the gesture worklet
+            runOnJS(updateVideo)('Ll0RattR1DE'); // Change when we make an API call
         });
     const composed = Gesture.Simultaneous(flingUp, flingDown)
 
